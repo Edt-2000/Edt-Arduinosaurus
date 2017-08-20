9d3a87f8970c02b91c416281e7f3440e8f50225b
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2017-08-20 16:53:23 +0200

    Initial commit (copied from Playground + minor changes)

---
+++ Arduino/libraries/Edt-lib/Top.h
@@ -0,0 +1,200 @@
+#pragma once
+
+#include <OSCArduino.h>
+#include <FastLED.h>
+#include <FastLEDColorScheduler.h>
+
+class EdtTOP : public OSC::IMessageConsumer
+{
+public:
+	FastLEDColorScheduler colorScheduler;
+	
+	EdtTOP(const char * pattern, uint8_t const nrOfLeds) {
+		_pattern = pattern;
+
+		_nrOfLeds = nrOfLeds;
+		_leds = new CRGB[nrOfLeds];
+
+		colorScheduler = FastLEDColorScheduler(_leds, nrOfLeds);
+	}
+
+	template<uint8_t dataPin, uint8_t clockPin>	void configurePins() {
+		FastLED.addLeds<APA102, dataPin, clockPin, BRG>(_leds, _nrOfLeds);
+	}
+
+	const char * address() {
+		return _pattern;
+	}
+
+	void callback(OSC::Message * msg) {
+		_mode = (Mode)msg->getInt(0);
+
+		switch (_mode) {
+
+		case SinglePulse:
+		case SingleSolid:
+
+			_start = (msg->getInt(1) / 127.0) * _nrOfLeds;
+			_end = (msg->getInt(2) / 127.0) * _nrOfLeds;
+
+			_color1.h = msg->getInt(3);
+			_color1.s = msg->getInt(4);
+			_color1.l = msg->getInt(5);
+
+			if (_color1.l > 0) {
+
+				fill_solid(_leds + _start, _end - _start, _color1.chsv());
+			}
+
+			if (_mode == SinglePulse || _color1.l == 0) {
+				_duration = msg->getInt(6);
+
+				colorScheduler.blackout(_start, _end, _duration);
+			}
+			else {
+				colorScheduler.disableBlackout(_start, _end);
+			}
+
+			break;
+
+		case RainbowPulse:
+		case RainbowSolid:
+
+			_start = (msg->getInt(1) / 127.0) * _nrOfLeds;
+			_end = (msg->getInt(2) / 127.0) * _nrOfLeds;
+			
+			_hue = msg->getInt(3);
+			_deltahue = (msg->getInt(4) / 127.0) * (255.0 / _nrOfLeds);
+
+			if (_deltahue > 0) {
+				fill_rainbow(&_leds[_start], _end - _start, _hue, _deltahue);
+			}
+
+			if (_mode == RainbowPulse || _deltahue == 0) {
+				_duration = msg->getInt(5);
+
+				colorScheduler.blackout(_start, _end, _duration);
+			}
+			else {
+				colorScheduler.disableBlackout(_start, _end);
+			}
+
+			break;
+
+
+		case VUMeter:
+
+			_start = (msg->getInt(1) / 127.0) * _nrOfLeds;
+			_center = (msg->getInt(3) / 127.0) * _nrOfLeds;
+			_end = (msg->getInt(2) / 127.0) * _nrOfLeds;
+
+			_hue = msg->getInt(4);
+			_deltahue = msg->getInt(5);
+			_intensity = (float)(msg->getInt(6));
+
+			if (_start != _center) {
+				
+				int leds = (_center - _start) * (_intensity / 255.0);
+
+				colorScheduler.blackout(_start, _center - leds, 127);
+				colorScheduler.disableBlackout(_center - leds, _center);
+
+				fill_rainbow_reverse(&_leds[_center - leds - 1], leds, _hue, _deltahue / (_center - _start));
+			}
+
+			if (_center != _end) {
+
+				int leds = (_end - _center) * (_intensity / 255.0);
+
+				colorScheduler.blackout(_end - (_end - _center - leds), _end, 127);
+				colorScheduler.disableBlackout(_center, _center + leds);
+
+				fill_rainbow(&_leds[_center], leds, _hue, _deltahue / (_end - _center));
+			}
+
+			break;
+
+		case Twinkle:
+		
+			_start = (msg->getInt(1) / 127.0) * _nrOfLeds;
+			_end = (msg->getInt(2) / 127.0) * _nrOfLeds;
+
+			colorScheduler.disableBlackout(_start, _end);
+
+			_color1.h = msg->getInt(3);
+			_color1.s = 255;
+			_color1.l = 255;
+			_intensity = (float)(msg->getInt(4));
+
+			_color2.l = 0;
+
+			if (_intensity > 0) {
+
+				for (int i = _start; i < _end; i++) {
+					if (_intensity > random8()) {
+						_leds[i] = _color1.chsv();
+					}
+					else {
+						_leds[i] = _color2.chsv();
+					}
+				}
+			}
+			else {
+				colorScheduler.blackout(_start, _end, 127);
+			}
+
+			break;
+
+		case Strobo:
+
+			// stobo only has 2 parameters
+			_hue = msg->getInt(1);
+			_intensity = msg->getInt(2);
+
+			colorScheduler.strobo(_hue, _intensity);
+
+			break;
+		}
+	}
+
+	void animationLoop() {
+		
+		colorScheduler.loop();
+	}
+private:
+	enum Mode {
+		SingleSolid = 1,
+		SinglePulse = 2,
+		RainbowSolid = 3,
+		RainbowPulse = 4,
+		VUMeter = 100,
+		Twinkle = 101,
+		Strobo = 200
+	};
+
+	struct Color {
+		int h;
+		int s;
+		int l;
+
+		CHSV chsv() {
+			return CHSV(h, s, l);
+		}
+	};
+
+	const char * _pattern;
+
+	CRGB *_leds;
+
+	Color _color1;
+	Color _color2;
+	int _mode;
+	int _start;
+	int _end;
+	int _center;
+	int _hue;
+	int _deltahue;
+	int _duration;
+	int _nrOfLeds;
+	float _intensity;
+};
---
+++ README.md
@@ -0,0 +1,44 @@
+# Ed-2000
+Arduino &amp; GameTrak based expressive midi controller
+
+>This is our playground for all our code and experiments, we are in the progress of making separate git repositories for released code. Have a look at the [Edt-2000 organisation](https://github.com/Edt-2000) for all the repositories.
+
+## OSC lighting commands
+
+start = 0 - 127 (0 = begin, 127 = end)
+end = 0 - 127
+
+### Single color, solid
+/address 1 [start: int] [end: int] [h: int] [s: int] [l: int] [duration (only required with l == 0): int]
+
+l == 0 dims using pulse
+
+### Single color, pulse
+/address 2 [start: int] [end: int] [h: int] [s: int] [l: int] [duration: int]
+
+### Rainbow color, solid
+/address 3 [start: int] [end: int] [h start: int] [delta h: int] [duration (only required with delta h == 0): int]
+
+delta h == 0 dims using pulse
+delta h == 127 = full rainbow
+delta h == 255 = two full rainbows
+
+### Rainbow color, pulse
+/address 4 [start: int] [end: int] [h start: int] [delta h: int] [duration: int]
+
+### VU Meter
+/address 100 [start: int] [end: int] [center: int] [h start: int] [delta h: int] [intensity: int]
+
+### Strobo
+/address 200 [h: int] [fps: int]
+
+h == 0 yields white strobo
+fps == 0 stops strobo
+
+## License
+
+Details about the licensing can be found in the LICENSE file.
+
+* Please use this code for your own DIY projects
+* When you make interesting changes and additions; send us a message and/or make a Pull Request
+* Send us a link to your blog if you have one, we would love others to use our work to create cool live performances

242d0183289cbbb1baa53be53b469b9102b4ea96
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2018-01-09 21:45:19 +0100

    Merge
    Merge
    
    
    Merge branch 'test'

--- Arduino/libraries/Edt-app/LEDApplication.h
+++ Arduino/libraries/Edt-app/LEDApplication.h
@@ -1,10 +1,13 @@
 #pragma once
 
+//#define USB
+
 #include "AbstractApplication.h"
 
 #include "Definitions.h"
 
 #include "Arduino.h"
+#include "HardwareSerial.h"
 #include "Ethernet.h"
 #include "EthernetUdp.h"
 #include "OSCArduino.h"
@@ -28,18 +31,36 @@ class LEDApplication : public AbstractApplication
 	//EdtLED led5 = EdtLED(OSC_ONOFFLED5, 11);
 	//EdtLED led6 = EdtLED(OSC_ONOFFLED6, 13);
 
+#ifndef USB
 	EthernetUDP udp;
+#endif
 
 	void setupStatus()
 	{
+		
+
 		status.setup(13, HIGH);
 	}
 
 	void setupNetwork()
 	{
+#ifdef USB
+
+		Serial.begin(9600);
+
+		while(!Serial) {
+			;
+		}
+
+		Serial.println("Hi");
+
+#else
+
 		Ethernet.begin(MAC_LED);
 
 		udp.begin(PORT_BROADCAST);
+
+#endif;
 	}
 
 	void setupOsc()
@@ -49,10 +70,14 @@ class LEDApplication : public AbstractApplication
 		rgbLed3.configurePins<A2, A5>();
 
 		osc = OSC::Arduino(3, 0);
+#ifdef USB
+		osc.bindStream(&Serial);
+#else
 		osc.bindUDP(&udp, IP_BROADCAST, PORT_BROADCAST);
+#endif;
 		osc.addConsumer(&rgbLed1);
-		osc.addConsumer(&rgbLed2);
-		osc.addConsumer(&rgbLed3);
+		//osc.addConsumer(&rgbLed2);
+		//osc.addConsumer(&rgbLed3);
 		// osc.addConsumer(&led1);
 		// osc.addConsumer(&led2);
 		// osc.addConsumer(&led3);
